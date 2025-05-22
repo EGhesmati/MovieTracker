f2addd2029d5a2fabe9b4bbc87e0983969da5884
EGhesmati <your-email@example.com>
2025-05-22 22:10:41 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'  // Correctly import the plugin
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()], // Use the plugin in the plugins array
-base: 'MovieTracker/',
+  plugins: [react(), svgr()],
+  base: '/MovieTracker/',  // Add slashes before and after
 })
+
